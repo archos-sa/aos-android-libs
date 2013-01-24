ad4e635354ee0460ef7becd73ed430e08530f679
Francois Caron <caron@archos.com>
2013-01-24 10:44:33 +0100

    Import libnativehelper lib

---
+++ dump-libs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+dump_symbols()
+{
+	nm -D $1 | awk '{print $3}' | grep -v '^__aeabi\|__FINI_ARRAY__\|__INIT_ARRAY__\|__dso_handle'
+}
+
+rel=common
+for i in libnativehelper;do
+	echo "// generated C file; do not modify; see dump-libs.sh" > $rel/$i.c
+	for s in `dump_symbols $rel/$i.so`; do echo "void $s() {}" >> $rel/$i.c; done
+done
