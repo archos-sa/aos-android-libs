50994cdcb758dc16adef065828a1773442010af3
Thomas Guillem <guillem@archos.com>
2013-01-28 16:10:12 +0100

    fix override warning if included more than one time.

--- ndkbuild.mk
+++ ndkbuild.mk
@@ -20,7 +20,6 @@ COMMON_LIBS := \
 	libssl \
 	libcrypto
 
-
 define gen_lib
 $(TARGET_OUT)/$(1).so: $(ANDROID_LIBS)/common/$(1).c
 	$(TARGET_CC) $(ANDROID_LIBS)/common/$(1).c -shared -o $(TARGET_OUT)/$(1).so --sysroot=${NDK_ROOT}/platforms/android-9/arch-$(TARGET_ARCH)
@@ -28,5 +27,8 @@ endef
 
 $(TARGET_OUT)/$(LOCAL_MODULE).so: $(addsuffix .so, $(addprefix $(TARGET_OUT)/, $(COMMON_LIBS)))
 
+ifndef ANDROID_LIBS_GEN_$(TARGET_ARCH_ABI)
 $(foreach LIB,$(COMMON_LIBS),$(eval $(call gen_lib,$(LIB))))
+endif
 
+ANDROID_LIBS_GEN_$(TARGET_ARCH_ABI) := 1
