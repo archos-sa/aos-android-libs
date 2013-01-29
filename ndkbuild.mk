1097f2a30f17ba0f24ecaca980c8d5043591d191
Thomas Guillem <guillem@archos.com>
2013-01-29 16:48:56 +0100

    use BUILD_ANDROID_LIBS

--- ndkbuild.mk
+++ ndkbuild.mk
@@ -2,7 +2,7 @@ ifeq (,$(LOCAL_MODULE))
 $(error LOCAL_MODULE not defined)
 endif
 
-ANDROID_LIBS := $(REPO_TOP_DIR)/android-libs
+ANDROID_LIBS_DIR := $(dir $(BUILD_ANDROID_LIBS))
 
 COMMON_LIBS := \
 	libutils \
@@ -21,8 +21,8 @@ COMMON_LIBS := \
 	libcrypto
 
 define gen_lib
-$(TARGET_OUT)/$(1).so: $(ANDROID_LIBS)/common/$(1).c
-	$(TARGET_CC) $(ANDROID_LIBS)/common/$(1).c -shared -o $(TARGET_OUT)/$(1).so --sysroot=${NDK_ROOT}/platforms/android-9/arch-$(TARGET_ARCH)
+$(TARGET_OUT)/$(1).so: $(ANDROID_LIBS_DIR)/common/$(1).c
+	$(TARGET_CC) $(ANDROID_LIBS_DIR)/common/$(1).c -shared -o $(TARGET_OUT)/$(1).so --sysroot=${NDK_ROOT}/platforms/android-9/arch-$(TARGET_ARCH)
 endef
 
 $(TARGET_OUT)/$(LOCAL_MODULE).so: $(addsuffix .so, $(addprefix $(TARGET_OUT)/, $(COMMON_LIBS)))
